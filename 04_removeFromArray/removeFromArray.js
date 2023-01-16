654158536888a5a2368e2861221dd8adcc713a57
Geon Youn <geon.youn@outlook.com>
2023-01-16 10:39:17 -0500

    Complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...idx) {
+    let ret = [];
+    for (let i of arr) {
+        if (idx.includes(i)) continue;
+        ret.push(i);
+    }
+    return ret;
 };
 
 // Do not edit below this line
