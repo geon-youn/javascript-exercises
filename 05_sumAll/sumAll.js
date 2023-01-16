64b14fba97de2562decb62f68cae518d007fea2e
Geon Youn <geon.youn@outlook.com>
2023-01-16 10:43:46 -0500

    Complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function (a, b) {
+    if (typeof a != "number" || typeof b != "number" || a < 0 || b < 0) {
+        return 'ERROR'
+    }
+    if (a > b) {
+        let t = b;
+        b = a;
+        a = t;
+    }
+    let sum = 0;
+    for (let i = a; i <= b; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
