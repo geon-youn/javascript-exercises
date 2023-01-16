caaea18868c8cc079ee888c5528394acf888e55d
Geon Youn <geon.youn@outlook.com>
2023-01-16 10:25:22 -0500

    Complete 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(str, n) {
+    if (n < 0) return "ERROR";
+    
+    let ret = "";
+    for (let i = 0; i < n; i++) {
+        ret += str;
+    }
+    return ret;
 };
 
 // Do not edit below this line
