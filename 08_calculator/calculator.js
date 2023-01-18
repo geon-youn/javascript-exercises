15aa8abb96401edd484f35dad1473b93c7d54c43
Geon Youn <geon.youn@outlook.com>
2023-01-18 18:45:19 -0500

    Complete 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+	return a.reduce((total, i) => total += i, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  return a.reduce((total, i) => total *= i, 1);
 };
 
-const power = function() {
-	
+const power = function(m, e) {
+  return m ** e;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	let fact = 1;
+  for (let i = 2; i <= n; i++) {
+    fact *= i;
+  }
+  return fact;
 };
 
 // Do not edit below this line
