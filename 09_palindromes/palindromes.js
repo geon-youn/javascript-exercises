aa1365dc6f559ec47aa927d8292abe36b72808c4
Geon Youn <geon.youn@outlook.com>
2023-01-18 18:58:15 -0500

    Compelte 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const newStr = str.replace(/\W/g, '').toLowerCase();
+    let mid = Math.round(newStr.length / 2);
+    for (let i = 0; i < mid; i++) {
+        if (newStr.charAt(i) !== newStr.charAt(newStr.length - i - 1)) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
