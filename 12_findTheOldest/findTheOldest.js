1716a863ff71935d859378224a960cfe18315fd0
Geon Youn <geon.youn@outlook.com>
2023-01-18 19:16:39 -0500

    Complete 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,5 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(ppl) {
+    return ppl.reduce((curr, next) => curr = (next.yearOfDeath ?? (new Date()).getFullYear()) - next.yearOfBirth > (curr.yearOfDeath ?? (new Date()).getFullYear()) - curr.yearOfBirth ? next : curr, ppl[0]);
 };
 
 // Do not edit below this line
