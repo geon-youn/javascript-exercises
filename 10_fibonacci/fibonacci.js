3246801ad61bf572ba4b2b1f5a433dd238872334
Geon Youn <geon.youn@outlook.com>
2023-01-18 19:05:07 -0500

    Complete 10_fibonnaci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+    if (n < 0) {
+        return "OOPS";
+    }
 
+    let preprev = 0;
+    let prev = 1;
+    let current = 1;
+    for (let i = 1; i < n; i++) {
+        current = prev + preprev;
+        preprev = prev;
+        prev = current;
+    }
+
+    return current;
 };
 
 // Do not edit below this line
